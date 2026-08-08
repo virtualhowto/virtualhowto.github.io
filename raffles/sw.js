de7683e9b1fe8625678fc68ec930ed891cdc8630
Scott McGregor <scottm@hyperscott.com>
2026-08-08 11:46:35 +1000

    Add offline cache

---
+++ raffles/sw.js
@@ -0,0 +1,1 @@
+const CACHE='raffles-v1';const ASSETS=['./','index.html','data.json','manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)))})
